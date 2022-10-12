abe1a0607842d981824415fab05eb89278784314
Saworieza <saworieza@gmail.com>
2022-10-12 13:08:32 +0300

    nav work

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import styles from './style';
+import styles from "./style";
+
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
+// import Navbar from './components/Navbar'
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        NavBar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients /> 
+        <CTA /> 
+        <Footer /> 
       </div>
     </div>
 
