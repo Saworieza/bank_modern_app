26a6f4d4884af7b352318e1c1ac84e096e5e06e9
Saworieza <saworieza@gmail.com>
2022-10-15 13:45:26 +0300

    minor changes on clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,4 +1,4 @@
-import {clients} from '../components';
+import {clients} from '../constants';
 import styles from '../style';
 
 
@@ -6,7 +6,7 @@ const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client) => (
-        <div key='cleint.id' className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+        <div key='client.id' className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
           <img src={client.logo} alt='client' className='sm:w-[192] w-[100px] object-contain' />
         </div>
       ))}
