d7813fa393fff437991a3f82b15b5965d17fed65
Saworieza <saworieza@gmail.com>
2022-10-16 13:42:37 +0300

    cleared the footer errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import {logo} from '../assets';
 import {footerLinks, socialMedia} from '../constants';
 
 const Footer = () => (
-  <section id='' className={`${styles.flexCenter} ${styles.paddingY} flexx-full`}>
+  <section id='' className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className='flex-1 flex flex-col justify-start mr-10'>
-        <img src={logo} alt='logo' className='w-[260px] h-[72px] object-contain' />
+        <img src={logo} alt='logo' className='w-[266px] h-[72px] object-contain' />
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make payments, easy reliable and secure.</p>
       </div>
       
@@ -27,23 +27,27 @@ const Footer = () => (
         ))}
       </div>
     </div>
-
-    <div className='w-full flex  justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
-      <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
-        2021 HooBank. All Rights Reserved.
+          
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        Copyright Ⓒ 2022 HooBank. All Rights Reserved.
       </p>
-      <div clasName='flex flex-row md:mt-0 mt-6'>
-        {socialMedia.map((social, index)=>(
-          <img 
+
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img
             key={social.id}
             src={social.icon}
             alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+            }`}
+            onClick={() => window.open(social.link)}
           />
         ))}
       </div>
     </div>
   </section>
-)
+);
 
 export default Footer
